e0dc4531368846801d506eceb19d66b8d6e08e81
dahae8 <dahae8@naver.com>
2023-09-18 00:44:44 +0900

    :bulb: Feat: 유튜브 키워드 데이터 전처리 기능 추가

--- bigdata/dataAnalysis/keyword/keywordAnalysis.py
+++ bigdata/dataAnalysis/keyword/keywordAnalysis.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from sqlalchemy import create_engine
+
+
+# MySQL 데이터베이스 연결 설정
+db_username = 'root'
+db_password = '1234'
+db_host = 'localhost'
+db_port = '3306'
+db_name = 'test'
+
+# MySQL 연결 문자열 생성
+connection_str = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+# MySQL 엔진 생성
+engine = create_engine(connection_str)
 
 # CSV 파일이 있는 디렉토리 경로
-csv_file_ad_detail = 'C:/Users/SSAFY/Desktop/adRec/data/keyword/AiSAC 광고소재명별 광고 정보.csv'
-csv_file_ad_keyword = 'C:/Users/SSAFY/Desktop/adRec/data/keyword/AiSAC시스템 AI 인식결과 및 광고소재명별 키워드.csv'
+csv_file_ad_detail = 'S09P22C107/bigdata/dataAnalysis/keyword/data/AiSAC 광고소재명별 광고 정보.csv'
+csv_file_ad_keyword = 'S09P22C107/bigdata/dataAnalysis/keyword/data/AiSAC시스템 AI 인식결과 및 광고소재명별 키워드.csv'
 
 csv_files = [csv_file_ad_detail, csv_file_ad_keyword]
 
@@ -21,54 +36,58 @@ for row_num_ad_detail, row_ad_detail in dataframe_ad_detail.iterrows():
     large_product = row_ad_detail['대업종 분류']
     middle_product = row_ad_detail['중업종 분류']
     small_product = row_ad_detail['소업종 분류']
-    data_key = large_product + ','+middle_product + ','+small_product
+    data_key = large_product + ' '+middle_product + ' '+small_product
 
     for row_num_ad_keyword, row_ad_keyword in dataframe_ad_keyword[idx:].iterrows():
+
+        # if row_num_ad_keyword >10:
+        #     break
 
         if row_ad_detail['광고소재명'] == row_ad_keyword['광고소재명']:
             keywords = [word.strip("' ").strip()
                         for word in row_ad_keyword['키워드'].split(',')]
+            
             for keyword in keywords:
-                data_key += ',' + keyword
+                data_key = large_product + ','+middle_product + ','+small_product + ','+keyword
                 if data_key in data:
-                    data[data_key] += 1
+                    data[data_key]+= 1
                 else:
                     data[data_key] = 1
             idx += 1
             break
-print(data)
-# styled_df = df.style
-# styled_df.set_table_styles(
-#     [{'selector': 'th', 'props': [('text-align', 'center')]}])  # 테이블 스타일 지정
-# styled_df.set_caption('Sample DataFrame')  # 표 제목 추가
-# styled_df.set_precision(2)  # 소수점 이하 자릿수 설정
-# styled_df.hide_index()  # 인덱스 열 숨김
-# styled_df.hide_columns()  # 특정 열 숨김
 
 
-# for row_num, row in dataframe_ad_keyword.iterrows():
-#     if start_row <= row_num <= end_row:
-#         # 읽어온 행을 출력하거나 다른 작업을 수행합니다.
-#         print(row)
-#         b = row[-1]
-#         a = [word.strip("' ").strip() for word in b.split(',')]
-#         print(a)
-#     elif row_num > end_row:
-#         # 종료 행을 넘어가면 반복문을 종료합니다.
-#         break
 
-# # CSV 파일을 읽기 모드로 엽니다.
-# with open(csv_file, mode='r', newline='') as file:
-#     # CSV 파일을 읽는 데 사용할 CSV 리더 객체를 생성합니다.
-#     csv_reader = csv.reader(file)
+# CREATE TABLE `adKeyword` (
+# 	`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+# 	`name`	VARCHAR(80)	NOT NULL,
+# 	`total`	INT	NOT NULL,
+# 	`productSmall_id`	INT	NOT NULL
+# );
 
-#     # 특정 범위의 행을 읽어옵니다.
-#     for row_num, row in enumerate(csv_reader, start=1):
-#         if start_row <= row_num <= end_row:
-#             # 읽어온 행을 출력하거나 다른 작업을 수행합니다.
-#             b = row[-1]
-#             a = [word.strip("' ").strip() for word in b.split(',')]
-#             print(a)
-#         elif row_num > end_row:
-#             # 종료 행을 넘어가면 반복문을 종료합니다.
-#             break
+sql_data = {
+    'productSmall_id1': [],
+    'productSmall_id2': [],
+    'productSmall_id': [],
+    'name': [],
+    'total': [],
+}
+
+for key, value in data.items():
+     product_lst = [word.strip("' ").strip() for word in key.split(',')]
+     sql_data['productSmall_id1'].append(product_lst[0])
+     sql_data['productSmall_id2'].append(product_lst[1])
+     sql_data['productSmall_id'].append(product_lst[2])
+     sql_data['name'].append(product_lst[3])
+     sql_data['total'].append(value)
+    
+df = pd.DataFrame(sql_data)
+# DataFrame 출력
+# print(df)
+
+# DataFrame을 MySQL 테이블로 저장
+table_name = 'adKeyword'  # 저장할 테이블 이름
+df.to_sql(table_name, engine, if_exists='replace', index=False)
+
+# MySQL 연결 닫기
+engine.dispose()
---
+++ bigdata/dataAnalysis/trendKeyword/trendkeywordAnalysis.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# MySQL 데이터베이스 연결 설정
+db_username = 'root'
+db_password = '1234'
+db_host = 'localhost'
+db_port = '3306'
+db_name = 'test'
+
+# MySQL 연결 문자열 생성
+connection_str = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
+# MySQL 엔진 생성
+engine = create_engine(connection_str)
+
+
+# 폴더 경로 설정
+folder_path = 'S09P22C107/bigdata/dataAnalysis/trendKeyword/data/'
+
+# 폴더 내의 모든 파일 목록 가져오기
+file_list = os.listdir(folder_path)
+
+# XLSX 파일만 선택
+xlsx_files = [file for file in file_list if file.endswith('.xlsx')]
+
+
+data = {}
+
+for xlsx_file in xlsx_files:
+    file_path = os.path.join(folder_path, xlsx_file)
+    df = pd.read_excel(file_path)
+    # print("=====================\n")
+    # print(df)
+    
+    for row_num, row in df.iterrows():
+        data_key = row["tmng"]
+
+        if data_key =="- topic":
+            data_key = "topic"
+
+        if data_key in data:
+            data[data_key]+= 1
+        else:
+            data[data_key] = 1
+
+
+sql_data = {
+    'name': [],
+    'total': [],
+}
+
+for key, value in data.items():
+     sql_data['name'].append(key)
+     sql_data['total'].append(value)
+
+df = pd.DataFrame(sql_data)
+# DataFrame 출력
+# print(df)
+
+# DataFrame을 MySQL 테이블로 저장
+table_name = 'youtubeKeyword'  # 저장할 테이블 이름
+df.to_sql(table_name, engine, if_exists='replace', index=True)
+
+# MySQL 연결 닫기
+engine.dispose()
