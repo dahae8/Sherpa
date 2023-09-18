38d3501921d06c2f68a046a1ca968308d3c394da
양수완 <kjjs2670@naver.com>
2023-09-18 15:37:12 +0900

    :bulb: Feature : 로그인 및 회원가입 UI 구성

--- frontend/src/routes/Route.jsx
+++ frontend/src/routes/Route.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '../components/pages/MainPage/MainPage';
 import MediaRecommendPage from '../components/pages/MediaRecommendPage/MediaRecommendPage';
+import Auth from '../components/pages/Auth/Auth'
 
 function RouteLink(props) {
   return (
@@ -10,6 +11,7 @@ function RouteLink(props) {
         {/* <Route path="/login" element={<LoginPage />} /> */}
         {/* <Route path="/signup" element={<SignupPage />} /> */}
         {/* <Route path="/mypage" element={<MyPage />} /> */}
+        <Route path="/auth" element={<Auth />} />
         <Route path="/mediaRecommend" element={<MediaRecommendPage />} />
         {/* <Route path="/mediaResult/online" element={<MyPage />} /> */}
         {/* <Route path="/mediaResult/tv" element={<MyPage />} /> */}
