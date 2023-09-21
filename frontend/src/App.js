8f2c2f0f99a0bcde544f7fb756446289433c1dad
이민규 <lmg386411@gmail.com>
2023-09-21 11:35:54 +0900

    :zap: Patch: NabBar App에 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
-import RouteLink from './routes/Route';
+import React from "react";
+import "./App.css";
+import RouteLink from "./routes/Route";
+import NavBar from "./components/organisms/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <RouteLink></RouteLink>
     </div>
   );
--- frontend/src/components/pages/MediaRecommendPage/MediaRecommendPage.jsx
+++ frontend/src/components/pages/MediaRecommendPage/MediaRecommendPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import axios from "axios";
-import NavBar from "../../organisms/NavBar";
 
 export const MainPage = () => {
   return (
     <div>
-      <NavBar></NavBar>
       <h1>매체 추천</h1>
     </div>
   );
