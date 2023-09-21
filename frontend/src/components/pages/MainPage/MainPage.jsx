9c36bdd3d31cde67801743388ff3efbf3cc9fb68
이민규 <lmg386411@gmail.com>
2023-09-21 11:19:47 +0900

    :bulb: Feat: NavBar 요소들 라우터 경로 설정

--- frontend/src/components/pages/MainPage/MainPage.jsx
+++ frontend/src/components/pages/MainPage/MainPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "../../organisms/NavBar";
 
 export const MainPage = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <h1>메인페이지 입니다.</h1>
     </div>
   );
