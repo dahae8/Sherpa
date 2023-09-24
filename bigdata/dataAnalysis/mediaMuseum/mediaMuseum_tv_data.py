d20f69985ea849441d1b804cbab52087c1390175
손효민 <sondoh100@naver.com>
2023-09-24 15:50:01 +0900

    :label: Rename: productMedia에 tv 데이터 추가

--- bigdata/dataAnalysis/mediaMuseum/mediaMuseum_tv_data.py
+++ bigdata/dataAnalysis/mediaMuseum/mediaMuseum_tv_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import mysql.connector
 
 # TV - 영상 / 라디오 - 라디오 / 인쇄 - 인쇄 / 옥외 - 옥외 / SNS / 커뮤니티
-data_tv = pd.read_csv('./csv/한국방송광고진흥공사_광고박물관 소장 광고소재(영상광고) 현황_20220311 (1).csv', encoding='cp949', low_memory=False)
+data_tv = pd.read_csv('../csv/한국방송광고진흥공사_광고박물관 소장 광고소재(영상광고) 현황_20220311 (1).csv', encoding='cp949', low_memory=False)
 # print(data_tv)  # [17852 rows x 12 columns]
 
 # 안쓰는 컬럼들 정리
