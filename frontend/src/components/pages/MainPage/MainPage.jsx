7e564ba44cd608edc06f8e84f258dd06f8304a08
이민규 <lmg386411@gmail.com>
2023-09-26 16:11:21 +0900

    :bulb: Feat: 메인페이지 광고 대상, 매체 추천 소개

--- frontend/src/components/pages/MainPage/MainPage.jsx
+++ frontend/src/components/pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import SherpaMean from "../../organisms/SherpaMean";
 import IntroduceRecommendation from "../../organisms/IntroduceRecommendation";
+import IntroduceMediaRecommendation from "../../organisms/IntroduceMediaRecommendation";
 
 const Contanier = styled.div`
   margin: 0 320px;
@@ -12,6 +13,7 @@ export const MainPage = () => {
     <Contanier>
       <SherpaMean></SherpaMean>
       <IntroduceRecommendation></IntroduceRecommendation>
+      <IntroduceMediaRecommendation></IntroduceMediaRecommendation>
     </Contanier>
   );
 };
