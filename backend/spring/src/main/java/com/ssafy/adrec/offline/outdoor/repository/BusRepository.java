78f87657d84a82af45ccd5d40aecfb49dbd97aac
dahae8 <dahae8@naver.com>
2023-09-27 03:34:12 +0900

    :bulb: Feat: 버스 entity, repository, res, req 파일 추가

---
+++ backend/spring/src/main/java/com/ssafy/adrec/offline/outdoor/repository/BusRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.adrec.offline.outdoor.repository;
+
+import com.ssafy.adrec.offline.outdoor.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusRepository extends JpaRepository<Bus, Long> {
+
+}
