8b9b8a60cfa2b35d4f9b501664b5691a75b9858a
손효민 <sondoh100@naver.com>
2023-10-02 02:49:32 +0900

    :bulb: Feat: 광고 제작사 매칭 api

--- backend/spring/src/main/java/com/ssafy/adrec/media/service/MediaService.java
+++ backend/spring/src/main/java/com/ssafy/adrec/media/service/MediaService.java
@@ -1,6 +1,7 @@
 package com.ssafy.adrec.media.service;
 
 import com.ssafy.adrec.media.MediaTypes;
+import com.ssafy.adrec.media.response.CompanyGetRes;
 import com.ssafy.adrec.media.response.MediaSubGetRes;
 import com.ssafy.adrec.media.response.MediaTypeGetRes;
 
@@ -11,4 +12,6 @@ public interface MediaService {
     List<MediaTypeGetRes> getMediaTypeList(MediaTypes mediaTypes, Long id);
 
     List<MediaSubGetRes> getMediaSubList(MediaTypes mediaTypes, Long id);
+
+    List<CompanyGetRes> getCompanyList(Long mediaTypeId, Long mediaSubId);
 }
