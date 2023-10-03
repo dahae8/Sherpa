97c3cb6165cc89266e9b3b320486c907f03d0580
손효민 <sondoh100@naver.com>
2023-10-03 18:05:27 +0900

    :bulb: Feat: 광고 컨텐츠 추천 결과 저장 api

--- backend/spring/src/main/java/com/ssafy/adrec/media/repository/MediaSubRepository.java
+++ backend/spring/src/main/java/com/ssafy/adrec/media/repository/MediaSubRepository.java
@@ -4,7 +4,10 @@ import com.ssafy.adrec.media.MediaSub;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MediaSubRepository extends JpaRepository<MediaSub, Long> {
     List<MediaSub> findAllByMediaType_Id(Long mediaTypeid);
+
+    Optional<MediaSub> findById(Long id);
 }
