2c2ac3d71250164418d2b41a1595eac036bf3c3e
이정찬 <wjdcks1203@naver.com>
2023-10-06 10:18:11 +0900

    :bug: Fix: 시나리오 저장안되는 문제

--- frontend/src/components/pages/OutdoorRecommendation/OutdoorRecommendation.jsx
+++ frontend/src/components/pages/OutdoorRecommendation/OutdoorRecommendation.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect, useState, useLayoutEffect } from "react";
-import { useSelector } from "react-redux";
-import axios from "axios";
-import RecommendTarget from "../../organisms/RecommendTarget";
-import OfflineMediaRecommendation from "../../organisms/OfflineMediaRecommendation";
-import ChannelRecommendation from "../../organisms/ChannelRecommendation";
-import ProducerRecommendation from "../../organisms/ProducerCardList";
-import {
-  Container,
-  TargetBox,
-  Box,
-  Hr,
-  ProducerTitleItem,
-  ButtonBox,
-} from "./OutdoorRecommendation";
-import Button from "../../atoms/Button";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState, useLayoutEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import RecommendTarget from '../../organisms/RecommendTarget';
+import OfflineMediaRecommendation from '../../organisms/OfflineMediaRecommendation';
+import ChannelRecommendation from '../../organisms/ChannelRecommendation';
+import ProducerRecommendation from '../../organisms/ProducerCardList';
+import { Container, TargetBox, Box, Hr, ProducerTitleItem, ButtonBox } from './OutdoorRecommendation';
+import Button from '../../atoms/Button';
+import { useNavigate } from 'react-router-dom';
 
 const APPLICATION_FAST_SERVER_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://j9c107.p.ssafy.io"
-    : "http://j9c107.p.ssafy.io:8000";
+  process.env.NODE_ENV === 'production' ? 'https://j9c107.p.ssafy.io' : 'http://j9c107.p.ssafy.io:8000';
 
 const APPLICATION_SPRING_SERVER_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://j9c107.p.ssafy.io"
-    : "http://j9c107.p.ssafy.io:8080";
+  process.env.NODE_ENV === 'production' ? 'https://j9c107.p.ssafy.io' : 'http://j9c107.p.ssafy.io:8080';
 
 export const OutdoorRecommendation = () => {
   const navigate = useNavigate();
@@ -68,11 +57,11 @@ export const OutdoorRecommendation = () => {
   const recommendedMedia = useSelector((state) => state.result.media.recommend);
   const bigRegion = useSelector((state) => state.result.bigRegionName);
   const smallRegion = useSelector((state) => state.result.smallRegionName);
-  let target = "성별";
+  let target = '성별';
   if (gender === true) {
-    target = "남성";
+    target = '남성';
   } else {
-    target = "여성";
+    target = '여성';
   }
   const [subMediaLabels, setSubMediaLabels] = useState([]);
   const [subDatas, setSubDatas] = useState([]);
@@ -82,7 +71,7 @@ export const OutdoorRecommendation = () => {
   const [producerCardDatas2, setProducerCardDatas2] = useState({});
   const [producerCardDatas3, setProducerCardDatas3] = useState({});
   const [showProducer, setShowProducer] = useState(false);
-  const [recommendedRegion, setRecommendedRegion] = useState("");
+  const [recommendedRegion, setRecommendedRegion] = useState('');
   const [regionLabels, setRegionLabels] = useState([]);
   const [regionDatas, setRegionDatas] = useState([]);
   const [busLabels, setBusLabels] = useState([]);
@@ -99,20 +88,17 @@ export const OutdoorRecommendation = () => {
     console.log(APPLICATION_FAST_SERVER_URL);
     const recommendMedia = async () => {
       try {
-        console.log("서브 추천 api item", item);
-        console.log("서브 추천 api sigunguId", sigunguId);
-        console.log("서브 추천 api gender", gender);
-        console.log("서브 추천 api age", age);
-        const response = await axios.post(
-          `${APPLICATION_FAST_SERVER_URL}/fastapi/offline/product`,
-          {
-            productSmallId: item,
-            sigunguId: sigunguId,
-            gender: gender,
-            age: age,
-          }
-        );
-        console.log("서브 추천 매체 가져오기", response);
+        console.log('서브 추천 api item', item);
+        console.log('서브 추천 api sigunguId', sigunguId);
+        console.log('서브 추천 api gender', gender);
+        console.log('서브 추천 api age', age);
+        const response = await axios.post(`${APPLICATION_FAST_SERVER_URL}/fastapi/offline/product`, {
+          productSmallId: item,
+          sigunguId: sigunguId,
+          gender: gender,
+          age: age
+        });
+        console.log('서브 추천 매체 가져오기', response);
         const subMediaLabels = [];
         for (let i = 0; i < response.data.data.mediaList.length; i++) {
           if (response.data.data.mediaList[i]) {
@@ -132,18 +118,15 @@ export const OutdoorRecommendation = () => {
           setSubDatas(subDatas);
         }
       } catch (error) {
-        console.error("서브 추천 매체 가져오기 오류:", error);
+        console.error('서브 추천 매체 가져오기 오류:', error);
       }
     };
     const recommendPrice = async () => {
       try {
-        const response = await axios.post(
-          `${APPLICATION_FAST_SERVER_URL}/fastapi/offline/budget`,
-          {
-            budget: selectedPrice,
-          }
-        );
-        console.log("추천 가격 가져오기", response);
+        const response = await axios.post(`${APPLICATION_FAST_SERVER_URL}/fastapi/offline/budget`, {
+          budget: selectedPrice
+        });
+        console.log('추천 가격 가져오기', response);
         const priceLabels = [];
         for (let i = 0; i < response.data.data.budgetList.length; i++) {
           if (response.data.data.budgetList[i]) {
@@ -163,54 +146,45 @@ export const OutdoorRecommendation = () => {
           setPrices(prices);
         }
       } catch (error) {
-        console.error("추천 가격 가져오기 오류:", error);
+        console.error('추천 가격 가져오기 오류:', error);
       }
     };
     const linkproducer = async () => {
       try {
-        const response = await axios.get(
-          `${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=1`
-        );
-        console.log("제작사", response);
+        const response = await axios.get(`${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=1`);
+        console.log('제작사', response);
         setProducerCardDatas(response.data.data);
       } catch (error) {
-        console.log("제작사 오류", error);
+        console.log('제작사 오류', error);
       }
     };
     const linkproducer2 = async () => {
       try {
-        const response = await axios.get(
-          `${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=2`
-        );
-        console.log("제작사", response);
+        const response = await axios.get(`${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=2`);
+        console.log('제작사', response);
         setProducerCardDatas2(response.data.data);
       } catch (error) {
-        console.log("제작사 오류", error);
+        console.log('제작사 오류', error);
       }
     };
     const linkproducer3 = async () => {
       try {
-        const response = await axios.get(
-          `${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=3`
-        );
-        console.log("제작사", response);
+        const response = await axios.get(`${APPLICATION_SPRING_SERVER_URL}/api/media/company/6?mediaSubId=3`);
+        console.log('제작사', response);
         setProducerCardDatas3(response.data.data);
       } catch (error) {
-        console.log("제작사 오류", error);
+        console.log('제작사 오류', error);
       }
     };
     const recommendRegion = async () => {
       try {
-        const response = await axios.post(
-          `${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/area`,
-          {
-            listSize: 5,
-            gender: Number(gender),
-            age: age,
-            sigunguId: sigunguId,
-          }
-        );
-        console.log("장소 분석 가져오기", response);
+        const response = await axios.post(`${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/area`, {
+          listSize: 5,
+          gender: Number(gender),
+          age: age,
+          sigunguId: sigunguId
+        });
+        console.log('장소 분석 가져오기', response);
         setRecommendedRegion(response.data.data[0].type);
         const regionLabels = [];
         const regionDatas = [];
@@ -227,21 +201,18 @@ export const OutdoorRecommendation = () => {
         setRegionLabels(regionLabels);
         setRegionDatas(regionDatas);
       } catch (error) {
-        console.log("장소 분석 오류", error);
+        console.log('장소 분석 오류', error);
       }
     };
     const recommendBus = async () => {
       try {
-        const response = await axios.post(
-          `${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/bus`,
-          {
-            listSize: 5,
-            gender: Number(gender),
-            age: age,
-            sigunguId: sigunguId,
-          }
-        );
-        console.log("버스 가져오기", response);
+        const response = await axios.post(`${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/bus`, {
+          listSize: 5,
+          gender: Number(gender),
+          age: age,
+          sigunguId: sigunguId
+        });
+        console.log('버스 가져오기', response);
         const busLabels = [];
         const busDatas = [];
 
@@ -257,15 +228,13 @@ export const OutdoorRecommendation = () => {
         setBusLabels(busLabels);
         setBusDatas(busDatas);
       } catch (error) {
-        console.log("버스 오류", error);
+        console.log('버스 오류', error);
       }
     };
     const recommendSubway = async () => {
       try {
-        const response = await axios.get(
-          `${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/subway`
-        );
-        console.log("지하철 역 가져오기", response);
+        const response = await axios.get(`${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/subway`);
+        console.log('지하철 역 가져오기', response);
         const subwayLabels = [];
         const subwayDatas = [];
 
@@ -281,21 +250,18 @@ export const OutdoorRecommendation = () => {
         setSubwayLabels(subwayLabels);
         setSubwayDatas(subwayDatas);
       } catch (error) {
-        console.log("지하철 역 오류", error);
+        console.log('지하철 역 오류', error);
       }
     };
     const recommendOutdoor = async () => {
       try {
-        const response = await axios.post(
-          `${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/banner`,
-          {
-            listSize: 5,
-            gender: Number(gender),
-            age: age,
-            sigunguId: sigunguId,
-          }
-        );
-        console.log("현수막 가져오기", response);
+        const response = await axios.post(`${APPLICATION_SPRING_SERVER_URL}/api/offline/outdoor/banner`, {
+          listSize: 5,
+          gender: Number(gender),
+          age: age,
+          sigunguId: sigunguId
+        });
+        console.log('현수막 가져오기', response);
         const addresses = [];
 
         for (let i = 0; i < response.data.data.length; i++) {
@@ -306,9 +272,9 @@ export const OutdoorRecommendation = () => {
           }
         }
         setAddresses(addresses);
-        console.log("주소 리스트", addresses);
+        console.log('주소 리스트', addresses);
       } catch (error) {
-        console.log("현수막 오류", error);
+        console.log('현수막 오류', error);
       }
     };
     recommendMedia();
@@ -323,10 +289,10 @@ export const OutdoorRecommendation = () => {
   }, []);
 
   useEffect(() => {
-    const container = document.getElementById("myMap");
+    const container = document.getElementById('myMap');
     const options = {
       center: new kakao.maps.LatLng(35.1595454, 126.8526012),
-      level: 3,
+      level: 3
     };
     const map = new kakao.maps.Map(container, options);
 
@@ -339,7 +305,7 @@ export const OutdoorRecommendation = () => {
 
           new kakao.maps.Marker({
             map: map,
-            position: coords,
+            position: coords
           });
 
           map.setCenter(coords);
@@ -357,33 +323,24 @@ export const OutdoorRecommendation = () => {
 
   const save = async () => {
     try {
-      const response = await axios.post(
-        `${APPLICATION_SPRING_SERVER_URL}/api/mypage/save/mediaRec`,
-        {
-          memberName: name,
-          productSmallId: item,
-          budget: selectedPrice,
-          inOnOff: 1,
-          sigunguId: sigunguId,
-          mediaTypeId: 6,
-        }
-      );
-      console.log("저장 성공", response);
+      const response = await axios.post(`${APPLICATION_SPRING_SERVER_URL}/api/mypage/save/mediaRec`, {
+        memberName: name,
+        productSmallId: item,
+        budget: selectedPrice,
+        inOnOff: 1,
+        sigunguId: sigunguId,
+        mediaTypeId: 6
+      });
+      console.log('저장 성공', response);
     } catch (error) {
-      console.log("저장 오류", error);
+      console.log('저장 오류', error);
     }
   };
 
   return (
     <Container>
       <TargetBox>
-        <RecommendTarget
-          datas={ages}
-          target={target}
-          age={age}
-          male={male}
-          female={female}
-        ></RecommendTarget>
+        <RecommendTarget datas={ages} target={target} age={age} male={male} female={female}></RecommendTarget>
       </TargetBox>
       <Hr />
       <Box>
@@ -419,7 +376,7 @@ export const OutdoorRecommendation = () => {
       <Box>
         {subwayLabels.length > 0 && (
           <ChannelRecommendation
-            title="지하철 옥외 광고"
+            title="지하철 역 옥외 광고"
             datas={subwayDatas}
             labels={subwayLabels}
             description={`${bigRegion}에 있는 지하철 역 승하차량 통계`}
@@ -432,27 +389,21 @@ export const OutdoorRecommendation = () => {
         <div
           id="myMap"
           style={{
-            width: "100%",
-            height: "600px",
+            width: '100%',
+            height: '600px'
           }}
         ></div>
       </Box>
       <Hr />
       <Box>
         <ProducerTitleItem>버스 정류장 옥외 광고 제작사</ProducerTitleItem>
-        {showProducer && (
-          <ProducerRecommendation cardDatas={producerCardDatas} />
-        )}
+        {showProducer && <ProducerRecommendation cardDatas={producerCardDatas} />}
       </Box>
       <Box>
         <ProducerTitleItem>지하철 역 옥외 광고 제작사</ProducerTitleItem>
-        {showProducer && (
-          <ProducerRecommendation cardDatas={producerCardDatas2} />
-        )}
+        {showProducer && <ProducerRecommendation cardDatas={producerCardDatas2} />}
         <ProducerTitleItem>현수막 옥외 광고 제작사</ProducerTitleItem>
-        {showProducer && (
-          <ProducerRecommendation cardDatas={producerCardDatas3} />
-        )}
+        {showProducer && <ProducerRecommendation cardDatas={producerCardDatas3} />}
       </Box>
       <ButtonBox>
         <Button
@@ -464,7 +415,7 @@ export const OutdoorRecommendation = () => {
           fontSize="24px"
           onClick={() => {
             save();
-            navigate("/mypage");
+            navigate('/mypage');
           }}
         >
           보관함에 추가
@@ -476,7 +427,7 @@ export const OutdoorRecommendation = () => {
           textColor="white"
           fontSize="24px"
           onClick={() => {
-            navigate("/mediaRecommend");
+            navigate('/mediaRecommend');
           }}
         >
           다시 추천받기
